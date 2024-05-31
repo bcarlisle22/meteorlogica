8df5e0313aefb70aff2134d069ba3b5f5f7ce2eb
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2024-05-30 22:26:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
-// const apiKey = "3cf891d4f8f77dfb177e38f3c5c23a98";
+const APIKey = "58ebb3ff1cf28bab62fa83a63de5a348"; //error: "invalid API key"- added $ and got rid of city name, used city instead, changed API key to APIkey
 
-// const cityLocation = document.getElementById("location");
-// const cityTemp = document.getElementById("temperature");
+const cityLocation = document.getElementById("location");
+const cityTemp = document.getElementById("temperature");
 
+fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)    //error: 404 city not found;
+.then (response => response.json())
+.then (data => console.log(data))
+.catch (error => console.error('Error: ', error));
 
-// console.log(cityTemp);
-// console.log(location);
 
 const weatherBackground = document.getElementById("weatherPage");
 const weatherStatus = document.getElementById("weather");
 
 function changeBackgroundImage(){
-    let weatherStatus = "CLOUDY";
+    let weatherStatus = "RAIN";
 
     //sunny 
     if (weatherStatus == "SUNNY"){
         weatherBackground.style.backgroundImage = "url('images/bluesky-sunny.jpg')";
         document.getElementById("weather").innerHTML = "SUNNY";
+        // break;
 
     }
     //cloudy
     if (weatherStatus == "CLOUDY"){
         weatherBackground.style.backgroundImage = "url('images/cloudy.jpg')";
+        // images/cloudy-gif.mp4'
         document.getElementById("weather").innerHTML = "CLOUDY";
+        //break;
 
     }
     //rainy
     if (weatherStatus == "RAIN"){
         weatherBackground.style.backgroundImage = "url('images/rainy.jpg')";
         document.getElementById("weather").innerHTML = "RAIN";
+        //break;
 
     }
     //stormy
     if (weatherStatus == "STORM"){
         weatherBackground.style.backgroundImage = "url('images/storm-lightning.jpg')";
-        document.getElementById("weather").innerHTML = "THUNDER STORMS";
+        document.getElementById("weather").innerHTML = "SEVERE THUNDER WARNING";
+        window.alert("SEVERE THUNDERSTORM WARNING in effect for your area. Take shelter in a sturdy building, away from windows. Flying debris may be deadly to those caught without")
+        //break;
         
     }
     //tornado
     if (weatherStatus == "TORNADO"){
         weatherBackground.style.backgroundImage = "url('images/tornado-watch.jpg')";
-        document.getElementById("weather").innerHTML = "TORNADO WATCH";
+        document.getElementById("weather").innerHTML = "TORNADO WARNING";
+        window.alert("TORNADO WARNING in effect for your area. Take shelter now in a basement or an interior room on the lowest floor of a sturdy building.")
+        //break;
 
     }
     
